4f264299b6cdfef7545df5e5048c228187c6e516
Branden <brandenl8@gmail.com>
2021-04-08 15:56:58 +0900

    Fixed highlight nav listener and cleaned the page

--- JS/app.js
+++ JS/app.js
@@ -1,93 +1,74 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-/**
- * Define Global Variables
- * 
-*/
+// Define global variables
+const ulNav = document.querySelector("#navbar__list");
+const sect = document.querySelectorAll("section");
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-const ulNav = document.querySelector('#navbar__list');
-const sect = document.querySelectorAll('section');
-
-sect.forEach(function(navSect) {
-    const sectId = navSect.getAttribute('id');  
-    const someData = navSect.getAttribute('data-nav'); 
-    const aLink = document.createElement('a');  
-    aLink.innerHTML = someData; 
-    aLink.setAttribute('class', 'menu__link'); 
-    aLink.setAttribute("href", `#${sectId}`);  //need backticks (``)  not quotes (' ' or " ")
-    const makeNav = document.createElement('li'); 
-    makeNav.appendChild(aLink);
-    ulNav.appendChild(makeNav);
-})
-
-const scroll = new SmoothScroll('.navbar__menu a[href*="#"]',{
-    speed: 800
+// Build the nav
+sect.forEach(function (navSect) {
+  const sectId = navSect.getAttribute("id");
+  const someData = navSect.getAttribute("data-nav");
+  const aLink = document.createElement("a");
+  aLink.innerHTML = someData;
+  aLink.setAttribute("class", "menu__link");
+  aLink.setAttribute("href", `#${sectId}`);
+  const makeNav = document.createElement("li");
+  makeNav.appendChild(aLink);
+  ulNav.appendChild(makeNav);
 });
 
-//Function to check if which section is in the viewport
+const scroll = new SmoothScroll('.navbar__menu a[href*="#"]', {
+  speed: 800,
+});
+
+//Function to check which section is in the viewport
 function inViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return(
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
-const newNavs = document.querySelectorAll('a');
-
 // Add class 'active' to section when near top of viewport
-document.addEventListener('scroll',function () {
-    sect.forEach(current => {
-        newSectId = current.getAttribute("id");
-        inViewport(current) ? current.classList.add("active") : current.classList.remove("active");
+const newNavs = document.querySelectorAll("a");
+
+document.addEventListener("scroll", function () {
+  sect.forEach(
+    (current) => {
+      newSectId = current.getAttribute("id");
+      if (inViewport(current)) {
+        current.classList.add("active");
+        for (const link of newNavs) {
+          const aRef = link.getAttribute("href");
+          if (aRef === `#${newSectId}`) {
+            link.classList.add("nav__highlight");
+          } else {
+            link.classList.remove("nav__highlight");
+          }
+        }
+      } else {
+        current.classList.remove("active");
+      }
     },
     {
-        passive: true
-    }); 
+      passive: true,
+    }
+  );
 });
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
